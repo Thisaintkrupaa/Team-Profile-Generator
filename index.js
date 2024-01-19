e030dbb08ba36bfaba5e424d28bb803f81d75fca
Krupa Dulobo <115632825+Thisaintkrupaa@users.noreply.github.com>
2024-01-19 19:46:16 +0000

    Update index.js

--- index.js
+++ index.js
@@ -9,16 +9,45 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
+const { default: Choice } = require("inquirer/lib/objects/choice.js");
+const Engineer = require("./lib/Engineer");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+const teams = [];
 
-const managerQuestions = [ {
+const managerQuestions = [ 
+    
+    {
 
+    type: 'input',
+    name: 'name',
+    message:"Enter the teams managers name: ",
 
-}
+},
 
+{
+  type: 'input',
+    name: 'id',
+    message: "Enter the managers Employee ID: ",
 
+},
+
+{
+
+    type: 'input',
+    name: 'email',
+    message: "Enter the managers Email address: ",
+
+},
+
+{
+
+    type: 'input',
+    name: 'office number',
+    message: "Enter the managers office number: ",
+
+},
 
 
 ];
@@ -27,90 +56,127 @@ const managerQuestions = [ {
 
 const engineerQuestions = [ {
 
+    type:'input',
+    name: 'name',
+    message: "Enter the Engineer's name: ",
 
-}
+
+},
+
+{
+
+    type:'input',
+    name: 'id',
+    message: "Enter the engineers id: ",
 
 
+},
+
+{
+
+    type:'input',
+    name: 'email',
+    message: "Enter the engineers email address: ",
+
+
+},
+
+{
+
+    type:'input',
+    name: 'github',
+    message: "Enter the engineers github Username: ",
+
+},
 
 
 ];
 
-
-
 const internQuestions = [ {
 
+        type:'input',
+        name: 'name',
+        message: "Enter the interns name: ",
+    
+    
+    },
+    
+    {
 
-}
+        type:'input',
+        name: 'id',
+        message: "Enter the interns id: ",
+    
+    
+    },
 
+    {
 
+        type:'input',
+        name: 'email',
+        message: "Enter the interns Email address: ",
+    
+    
+    },
 
+    {
+
+        type:'input',
+        name: 'school',
+        message: "Enter the name of the interns school: ",
+    
+    },
 
 ]
-
-
-const team = [];
-
 //function to initialize the application
 
 const init = async () => {
-    let response = await inquirer.createPromptModule(managerQuestions)
+    let response = await inquirer.Prompt(managerQuestions)
     const manager = new Manager(
         response.name,
         response.id,
         response.email,
         response.officeNumber,
         );
+team.push (manager)
 
-        team.push (manager)
+while (response.addmember != "i dont want to add anymore members") {
+    if (response.addMember = 'Engineer') {
+        response = await inquirer.prompt(engineerQuestions)
+        const Engineer = new Engineer(
 
-        let addmore = true;
+            response.name,
+            response.id,
+            response.email,
+           response.github,
+        );
 
-        while (addmore) {
-            const{ choice} = await inquirer.createPromptModule([{
+        team.push (Engineer)
 
-                type: 'list',
-                name: 'chocie',
-                message: 'what type of',
-                choices: ''
-            },
-            ]);
+     } else if (response.addMember == 'Intern') {
 
-        if (choice === 'finish builiding the team') {
-            addmore = false;
-        } else{
-            const questions = 
-            choice === 'Engineer'
-        }
+        response = await inquirer.prompt(InternQuestions)
+        const Intern = new Intern(
+             response.name,
+             response.id,
+             response.email,
+            response.school
+        );
 
-
-
-
-
+        team.push(Intern)
+    }
+    
+    }
 }
-
 
 // call the render function to generate html 
 
 const html = render(teams);
 
 // Write Html file
-fs.writeFile('./outputh/teams.html');
 console.log('The tean has been succesfully created ', teams);
-
-}catch(error) {
-    console.error ('An error occured:' error);
-}
-
-
-
-
-// Function to initialize the application
-
-
-
-
+fs.writeFile('team.html', render(teams), (err) => err ? console.error (err) : console.log ('HTML file is created!'));
 
 //run the appplication
 
 init();
-
